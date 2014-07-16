db5129c9c00c5ad451384f9852ac49a84395a490
mellwa <kolachao.chen@gmail.com>
2014-07-16 17:05:13 -0400

    no message

--- rpc_client.cc
+++ rpc_client.cc
@@ -126,7 +126,7 @@ int clientHandleBinderResponse(int connectionSocket) {
         printf("Binder response: EXECUTE_SUCCESS\n");
         return 0;
     } else {
-        perror("No such a response type from binder to client!\n");
+        printf("No such a response type from binder to client! Type is: \n",responseType);
         return -1;
     }
 }
